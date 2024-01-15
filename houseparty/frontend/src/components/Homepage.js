f0c096cc6636fd626c7c64ce2b33a09ccf743e88
Miles Keaveny <mkeaveny@wesleyan.edu>
2024-01-15 14:19:07 -0500

    background

--- houseparty/frontend/src/components/Homepage.js
+++ houseparty/frontend/src/components/Homepage.js
@@ -5,6 +5,9 @@ import Home from "./Home";
 import Room from "./Room";
 import Info from "./Info";
 import {Grid, Typography, Button, ButtonGroup} from "@material-ui/core";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 
 
@@ -28,6 +31,8 @@ export default class Homepage extends Component {
         };
         this.clearRoomCode = this.clearRoomCode.bind(this);
     }
+
+    
 
     
 
@@ -102,7 +107,16 @@ export default class Homepage extends Component {
         
           renderHomePage(){
             return (
-              
+
+              <>
+              <Particles
+                     id="tsparticles"
+                     
+                     options={{ "fullScreen": {"enable": true, "zindex":-1}, "background":{ "image":" linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)" }, "particles":{ "number":{ "value":10, "density":{ "enable":false, "value_area":800 } }, "color":{ "value":"#ffffff" }, "shape": { "type": "square", "stroke":{ "width":0, "color":"#000000" }, "polygon":{ "nb_sides":5 } }, "opacity":{ "value":0.25, "random":true, "anim":{ "enable":false, "speed":1, "opacity_min":0.1, "sync":false } }, "size":{ "value":29, "random":true, "anim":{ "enable":false, "speed":2, "size_min":0.1, "sync":false } }, "line_linked":{ "enable":false, "distance":300, "color":"#ffffff", "opacity":0, "width":0 }, "move":{ "enable":true, "speed":0.5, "direction":"top", "straight":true, "out_mode":"out", "bounce":false, "attract":{ "enable":false, "rotateX":600, "rotateY":1200 } } }, "interactivity":{ "detect_on":"canvas", "events":{ "onhover":{ "enable":false, "mode":"repulse" }, "onclick":{ "enable":false, "mode":"push" }, "resize":true }, "modes":{ "grab":{ "distance":800, "line_linked":{ "opacity":1 } }, "bubble":{ "distance":790, "size":79, "duration":2, "opacity":0.8, "speed":3 }, "repulse":{ "distance":400, "duration":0.4 }, "push":{ "particles_nb":4 }, "remove":{ "particles_nb":2 } } }, "retina_detect":true}}
+                             />
+            
+     
+              <div className=" center">
               <Grid container spacing={3}>
                 <Grid item xs={12} align="center">
                   <Typography variant="h3" compact="h3">
@@ -123,6 +137,8 @@ export default class Homepage extends Component {
                   </ButtonGroup>
                 </Grid>
               </Grid>
+              </div>
+              </>
              
             );
           }
